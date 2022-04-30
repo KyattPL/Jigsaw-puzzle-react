ac16043ce7de496a07cea5d36eeb26ebc1923a6a
KyattPL <siema1234@gazeta.pl>
2022-04-30 22:26:47 +0200

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
+import { JigsawPuzzle } from "./JigsawPuzzle.tsx";
+import './JigsawPuzzle.css';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [timer, setTimer] = useState(0);
+    const [intervalId, setIntervalId] = useState(null);
+
+    const updateTime = () => {
+        setTimer(prev => prev + 1);
+    }
+
+    const solved = () => {
+        clearInterval(intervalId);
+        alert("You won!");
+    }
+
+    useEffect(() => {
+        console.log("i fire once");
+        let intId = setInterval(updateTime, 1000);
+        setIntervalId(intId);
+    }, []);
+
+    return (
+        <>
+        <JigsawPuzzle imageSrc="https://exumag.com/wp-content/uploads/2018/02/Grumpy-Cat-t%C5%82o-1170x659.jpg"
+            rows={4} columns={4} onSolved={solved}/>
+        <div className="puzzle-box"></div>
+        <div className="timer-box">{Math.floor(timer / 60)}min {timer % 60}s</div>
+        </>
+    );
 }
 
 export default App;
